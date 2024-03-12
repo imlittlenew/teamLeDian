52c86a455aecde97f125b4fa5455fa07b5c28708
TungShihChang <tung911443@gmail.com>
2024-03-12 14:02:09 +0800

    頁首頁尾完成

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,8 @@ import "../css/index.css";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import { PiMedal } from "react-icons/pi";
 import { PiCoins } from "react-icons/pi";
+import { GiCancel } from "react-icons/gi";
+
 
 class index extends Component {
     state = { 
@@ -15,11 +17,32 @@ class index extends Component {
                     <h4 id='homeBtn' className='my-auto btn headerText'>首頁</h4>
                     <h4 className='my-auto btn headerText menuBtn'><HiOutlineShoppingBag />購物車</h4>
                     <h4 className='my-auto btn headerText menuBtn'><PiMedal />品牌專區</h4>
-                    <h4 className='my-auto btn headerText menuBtn'><PiCoins />集點資訊</h4>
+                    <h4 className='my-auto btn headerText menuBtn' onClick={this.pointinfoShow}><PiCoins />集點資訊</h4>
+                </div>
+                <div id="pointinfo">
+                    <button  id="pointinfoclose" onClick={this.pointinfoHide}><GiCancel   className='fs-2    text-light' /></button>
+                    <h1>集點資訊</h1>
+                    <p>．每消費20元即可累積1點。</p>
+                    <p>．每點可折抵1元消費金額。</p>
+                    <p>．點數可在下次消費時折抵使用。</p>
+                    <p>．點數不可轉讓，不可兌換現金，不可合併使用。</p>
+                    <p>．本集點活動以本站公告為準，如有更改，恕不另行通知。</p>
                 </div>
                 <div className='d-flex me-2  align-items-center'>
-                    <h4 id='loginBtn' className='my-auto btn headerText'>登入/註冊▼</h4>
+                    <h4 id='loginBtn' className='my-auto btn headerText' onClick={this.toggleMemberNav}>登入/註冊▼</h4>
+                    <div id='memberNav' className='collapse'>
+                        <img id='memberNavImg' src={require("../images/LeDian_LOGO-05.png")} alt='logo'></img>
+                        <div>
+                            <h4 className='headerText my-2'>個人檔案</h4>
+                            <h4 className='headerText my-2'>帳號管理</h4>
+                            <h4 className='headerText my-2'>歷史訂單</h4>
+                            <h4 className='headerText my-2'>載具存取</h4>
+                        </div>
+                    </div>
                 </div>
+
+
+                
             </div>
             <div id='banner' className='d-flex justify-content-center'><img src={require("../images/Home_Banner_01.jpg")} alt='homeBanner' className='img-fluid'></img></div>
             <div className="container">
@@ -55,15 +78,26 @@ class index extends Component {
                         <p className='footerText m-0 py-1 text-nowrap text-white'>信用卡條款</p>
                     </div>
                 </div>
-
             </div>
             </React.Fragment>);
     }
-    searchChange = () => {
+    searchChange = (e) => {
         var newState = {...this.state};
-        newState.search = this.value;   
+        newState.search = e.target.value   
         this.setState(newState);
-        console.log(this.state.search)
+    }
+    pointinfoShow = function (event) {
+        document.getElementById("pointinfo").style.top = event.clientY + "px";
+        // document.getElementById("pointinfo").style.right = event.clientX + "px";
+    } 
+
+    pointinfoHide = function (event) {
+        document.getElementById("pointinfo").style.top = "-500px";
+        event.cancelBubble = true;
+    }
+
+    toggleMemberNav = () => {
+        document.getElementById('memberNav').classList.toggle('collapse');
     }
     test = () => {
         alert('ok')
